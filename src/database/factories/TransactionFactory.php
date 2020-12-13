a4ea8f6c05ba559d5ded78de775ce2b567102d34
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-13 19:12:18 +0000

    Add factories

---
+++ src/database/factories/TransactionFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Seller;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Transaction::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $seller = Seller::has('products')->get()->random();
+        $buyer = User::all()->except($seller->id)->random();
+
+        return [
+            'quantity' => $this->faker->word(), 
+            'seller_id' => $seller->id, 
+            'buyer_id' => $buyer->id, 
+            'product_id' => $seller->products->random()->id,
+        ];
+    }
+}
